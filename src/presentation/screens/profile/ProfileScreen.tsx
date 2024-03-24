3c95e3861b4229c78f6d60ab8c25ce3c9f1bd023
Carlos Carvajal <cxcarvaj@gmail.com>
2024-03-24 10:08:56 -0500

    Use Zustand to change values in store

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,34 @@
-import {Text, View} from 'react-native';
-import { styles } from '../../../config';
+import {Pressable, Text, View} from 'react-native';
+import {styles} from '../../../config';
+import {useProfileStore} from '../../store/profile-store.ts';
 
 export const ProfileScreen = () => {
+  const name = useProfileStore(state => state.name);
+  const email = useProfileStore(state => state.email);
+  const changeProfile = useProfileStore(state => state.changeProfile);
+
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.title}>{email}</Text>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => useProfileStore.setState({name: 'Carlos Carvajal'})}>
+        <Text>Change Name</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => useProfileStore.setState({email: 'cxcarvaj@gmail.com'})}>
+        <Text>Change Email</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => changeProfile('Jhon Doe', 'jhondoe@google.com')}>
+        <Text>Return to initial state</Text>
+      </Pressable>
     </View>
   );
 };
---
+++ src/presentation/store/profile-store.ts
@@ -0,0 +1,15 @@
+import {create} from 'zustand';
+
+export interface ProfileState {
+  name: string;
+  email: string;
+  changeProfile: (name: string, email: string) => void;
+}
+
+export const useProfileStore = create<ProfileState>()((set, get) => ({
+  name: 'John Doe',
+  email: 'jhondoe@google.com',
+  changeProfile: (name: string, email: string) => {
+    set({name, email});
+  },
+}));
